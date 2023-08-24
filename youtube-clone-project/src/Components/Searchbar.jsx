362389e7a2c79372664483e4cf1b0add4b43b4ae
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-23 20:29:31 -0400

    Add basic functionality to search bar.

--- youtube-clone-project/src/Components/Searchbar.jsx
+++ youtube-clone-project/src/Components/Searchbar.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
+const key = process.env.REACT_APP_KEY;
 export default function Searchbar() {
   const [query, setQuery] = useState("");
 
+  const handleQuery = (e) => {
+    e.preventDefault();
+    setQuery(e.target.value);
+  };
+  const searchVideos = () => {
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&q=${query}&key=${key}`
+    )
+      .then((results) => results.json())
+      .then((response) => console.log(response));
+  };
   return (
     <form>
-      <input type="text" value={query} />
+      <input type="text" value={query} onChange={handleQuery} />
     </form>
   );
 }
