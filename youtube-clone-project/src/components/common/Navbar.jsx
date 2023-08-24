4e802bcd9bb2d7e0b909f6506dedc9694d343f66
salvagebar <salvagebar@gmail.com>
2023-08-23 21:30:17 -0400

    added Bootstrap CSS, some styling on Navbar

--- youtube-clone-project/src/components/common/Navbar.jsx
+++ youtube-clone-project/src/components/common/Navbar.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div className='nav'>
-            <ul>
-                <li><h1>YouTOOB</h1></li>
-                <li>Home</li>
-                <li>About</li>
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div className="nav container">
+      <ul className="list-unstyled row">
+        <li>
+          <h1>YouTOOB</h1>
+        </li>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
