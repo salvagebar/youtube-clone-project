48ecb762772559236e1a1793ebe0fd5e25981f94
salvagebar <salvagebar@gmail.com>
2023-08-23 21:07:31 -0400

    created primitive Navbar, added components folder

---
+++ youtube-clone-project/src/components/common/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='nav'>
+            <ul>
+                <li><h1>YouTOOB</h1></li>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
